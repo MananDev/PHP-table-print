5a95f229d1e56e270599574b94a9bfaee8a7f458
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-02-07 12:22:53 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <html>
 <body>
- <script language="javascript">
-     const number = parseInt(prompt('Enter an integer: '));
- </script>
-<?php 
-  
-$num = 1;  
+
+<?php
+$num=$_POST['value'];
 for($i=1; $i<=10; $i++)   
 {
 $multiple = $i*$num;
