196df8fcdb008a0e92271a536f7606109d48c4cd
Muhammad Manan <muhammadmanan7175@gmail.com>
2022-02-10 22:03:42 +0500

    Update index.php
    adding box and button to get input from user

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <html>
 <body>
 
+<form Method="post">
+<br><br>
+Enter number:<input type="text" name="value"/><br><br>
+<input type="submit" name="btn" value="print table" />
+ 
+</form>
 <?php 
-$num= $_POST['Enter Number']
+if(isset($_POST['btn']))
+{
+$num=$_POST['value'];
 for($i=1; $i<=10; $i++)  
 {
 $multiple = $i*$num;
 echo "$num * $i = $multiple" ;   
 echo '<br>';	 
 }  
+}
 ?>  
 </body>
 </html>
-hckrherkhk
